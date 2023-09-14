beb5133d6712a451c6f762a5234f681b080d53eb
Matteojk95 <matteopalmer2095@gmail.com>
2023-09-14 18:29:24 +0200

    done

---
+++ src/main/java/org/learning/java/springpizzeria/Model/Pizza.java
@@ -0,0 +1,43 @@
+package org.learning.java.springpizzeria.Model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "pizza")
+
+public class Pizza {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String description;
+    private BigDecimal price;
+
+
+    //getter e setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
